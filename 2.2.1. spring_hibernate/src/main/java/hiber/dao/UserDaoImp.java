6274d8cc06663b3c52696524e42f15457792b29b
Ruslan <ruslan.mirzaev.work@gmail.com>
2022-11-06 23:09:16 +0300

    first commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,7 +5,9 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,6 +25,14 @@ public class UserDaoImp implements UserDao {
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public List<User> getUserByCar(String model, int series){
+      Query query = sessionFactory.getCurrentSession().createQuery("from User WHERE car.model = :model AND car.series = :series")
+              .setParameter("model", model)
+              .setParameter("series", series);
       return query.getResultList();
    }
 
